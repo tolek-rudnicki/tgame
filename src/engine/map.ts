f0ead8c481a7c26e03309c3e969ff73a0a77aff7
Tolek Rudnicki <tolek@epsi.pl>
2020-11-26 09:48:04 +0100

    Made comments so its easyer to read the code

--- src/engine/map.ts
+++ src/engine/map.ts
@@ -1,13 +1,21 @@
+// Buildings
 type Building = 'NONE' | 'FARM'
+
+// Areas that you can build in
+type Non_Building_Blocks = 'MOUNTAINS' | 'BEACH' | 'OCEAN'
+
+// Map
 type Field = {
-    kind: 'PLAINS' | 'DESERT' | 'MOUNTAINS' | 'WATER' | 'ICE' | 'CLAY';
+    kind: 'PLAINS' | 'DESERT' | 'ICE' | 'CLAY';
     building: Building;
 }
 
-const mapSize = 5;
+// Map size
+const mapSize = 10;
 type Map = Array<Array<Field>>;
 const map: Map = new Array<Array<Field>>();
 
+// Generate blocks untill hit the border (Map Size)
 for (let x = 0; x < mapSize; ++x) {
     map[x] = new Array<Field>();
     for (let y = 0; y < mapSize; ++y) {
@@ -17,9 +25,12 @@ for (let x = 0; x < mapSize; ++x) {
         }
     }
 }
+
+// I don't know
 const build = (map: Map, x: number, y: number, building: Building): void => {
     map[x][y].building = building
 }
 
-build(map, 2, 2, 'FARM')
+// Test building
+build(map, 3, 3, 'FARM')
 console.log(map)
