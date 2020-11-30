c1aa3b7e6b262f53ed8579c3df4b21b6df73973d
Tolek Rudnicki <tolek@epsi.pl>
2020-11-30 19:39:41 +0100

    Ocean generation, resources

--- src/engine/map.ts
+++ src/engine/map.ts
@@ -1,35 +1,87 @@
+// Biome
+type Biome = 'PLAINS' | 'ICE' | 'DESERT'
+
 // Landscape blocks
-type Block = 'PLAINS' | 'DESERT' | 'ICE' | 'CLAY' | 'MOUNTAINS' | 'BEACH' | 'OCEAN' | 'VOLCANO'
+type Block = 'NONE' | 'MOUNTAINS' | 'LAKE' | 'OCEAN'
 
 // Buildings
 type Building = 'NONE' | 'FARM'
 
+// Natural resource
+type Resource = 'NONE' | 'CRYSTALS' | 'ORE' | 'GAS'
+
 // Map
 type Field = {
     block: Block
+    resource: Resource
     building: Building
 }
 
-// Map size
-const mapSize = 5;
-type Map = Array<Array<Field>>;
+// Pick random integer
+const rand = (low: number, high: number): number => low + Math.floor(Math.random() * (high - low))
+const rand100 = (): number => Math.floor(Math.random() * 100)
 
-// Generate uniform map
-const init = (map: Map, size: number, block: Block): void => {
+// Map size
+const mapSize = 10
+type Map = Array<Array<Field>>
+
+const randomBlock = (): Block => {
+    const chance = rand100()
+    if (chance < 3.7) return 'LAKE'
+    else if (chance < 2 * 3.7) return 'MOUNTAINS'
+    return 'NONE'
+}
+
+const randomResource = (): Resource => {
+    const chance = rand100()
+    if (chance < 12.5) return 'CRYSTALS'
+    else if (chance < 2 * 12.5) return 'ORE'
+    else if (chance < 3 * 12.5) return 'GAS'
+    return 'NONE'
+}
+
+const putOcean = (map: Map, size: number, x: number, y: number, span: number): void => {
+    if (x < 1 || y < 1 || x > size - 1|| y > size -1) return;
+    map[x][y].block = 'OCEAN'
+    if (rand100() < 50) map[x - 1][y].block = 'OCEAN'
+    if (rand100() < 50) map[x + 1][y].block = 'OCEAN'
+    if (rand100() < 50) {
+        map[x][y - 1].block = 'OCEAN'
+        if (rand100() < 50) map[x - 1][y - 1].block = 'OCEAN'
+        if (rand100() < 50) map[x - 1][y + 1].block = 'OCEAN'
+    }
+    if (rand100() < 50) {
+        map[x][y + 1].block = 'OCEAN'
+        if (rand100() < 50) map[x + 1][y - 1].block = 'OCEAN'
+        if (rand100() < 50) map[x + 1][y + 1].block = 'OCEAN'
+    }
+    if (span) putOcean(map, size, x + rand(-1, 1), y + rand(-1, 1), span - 1)
+}
+
+// Generate random map
+const init = (map: Map, size: number, oceansCount: number, oceanSpan: number): void => {
     for (let x = 0; x < size; ++x) {
         map[x] = new Array<Field>()
         for (let y = 0; y < size; ++y) {
             map[x][y] = {
-                block,
-                building: 'NONE'
+                block: randomBlock(),
+                resource: randomResource(),
+                building: 'NONE',
             }
         }
     }
+
+    // Generate oceans
+    while (oceansCount--) {
+        const x = rand(1, size - 1)
+        const y = rand(1, size - 1)
+        putOcean(map, size, x, y, oceanSpan)
+    }
 }
 
-// I don't know
+// Put building on a map
 const build = (map: Map, x: number, y: number, building: Building): void => {
-    map[x][y].building = building
+    if (map[x][y].block == 'NONE' && map[x][y].building == 'NONE') map[x][y].building = building
 }
 
 const render = (map: Map, size: number): string => {
@@ -39,10 +91,10 @@ const render = (map: Map, size: number): string => {
         for (let y = 0; y < size; ++y) {
             let ch: string = " "
             switch (map[x][y].block) {
-                case 'PLAINS': ch = ","; break;
-                case 'DESERT': ch = "…"; break;
-                case 'ICE': ch = "^"; break;
-                case 'CLAY': ch = "#"; break;
+                case 'NONE': ch = "…"; break;
+                case 'MOUNTAINS': ch = "}"; break;
+                case 'LAKE': ch = "o"; break;
+                case 'OCEAN': ch = "~"; break;
             }
             for (let i = 0; i < tileSize; ++i) {
                 const offset = y * tileSize + i
@@ -57,12 +109,9 @@ const render = (map: Map, size: number): string => {
 
 // Test building
 const map: Map = new Array<Array<Field>>()
-init(map, mapSize, 'PLAINS')
+init(map, mapSize, 2, 5)
 build(map, 3, 3, 'FARM')
+
+// And give the output (for now)
 console.log(render(map, mapSize))
 
-// Here is ment to be a random number generator beetween 1 and 8.
-// And give the output (for now)
-
-// Structures that you can't build on have a 1/8 chanse of spawning
-// Ocean 1/15 1 or 2 oceans per map
