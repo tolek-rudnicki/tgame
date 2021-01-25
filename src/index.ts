a6028f15895df7f6c5ac20bc77e4dbda3daf0c64
Hit The Target <36575353+tolek-rudnicki@users.noreply.github.com>
2021-01-25 18:15:46 +0100

    Added electron

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
+import { app, BrowserWindow } from "electron"
+
 import { Farm } from "./buildings/Farm"
 import { Warehouse } from "./buildings/Warehouse";
 import { GameMap } from "./map/GameMap"
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+        nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
 
 const map = new GameMap('PLAINS', 10, 0, 5);
 const farm = new Farm();
